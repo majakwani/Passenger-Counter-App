90954867d2c890616437bfe0532da6f7b5a7e2bf
majakwani <anasjakwani72@gmail.com>
2022-11-12 15:17:25 +0500

    Wrote script for save button

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+let element = document.getElementById("counter")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
 function increment(){
-    let element = document.getElementById("counter")
-    count = element.innerText
-    count = parseInt(count)
     count = count + 1
     element.innerText = count
+}
+
+function save(){
+    let value = count + " - "
+    saveEl.textContent += value
+    element.innerText = 0
+    count = 0
 }
