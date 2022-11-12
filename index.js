206e7ee7404733a7b42a355f9eea6fb77a7662e9
majakwani <anasjakwani72@gmail.com>
2022-11-12 13:32:11 +0500

    Wrote a function for incrementing counter value

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.getElementById("counter").innerText = 5
+function increment(){
+    let element = document.getElementById("counter")
+    count = element.innerText
+    count = parseInt(count)
+    count = count + 1
+    element.innerText = count
+}
